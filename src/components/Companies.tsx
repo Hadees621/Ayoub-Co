a1cc88b0f1050024b5af0bdc9b15441f96b3388f
hadees <hadees621@gmail.com>
2024-03-17 15:40:46 +0500

    fix: marquee slider

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -16,7 +16,7 @@ const Companies = () => {
     });
 
     if (containerRef.current) {
-      containerRef.current.style.width = `${totalWidth * 2}px`; // Double the width to ensure seamless looping
+      containerRef.current.style.width = `${totalWidth * 2}px`;
     }
   }, [companies]);
 
@@ -36,35 +36,21 @@ const Companies = () => {
           .company-container {
             display: flex;
             overflow-x: hidden;
-            width: 100%;
-            animation: scrollAnimation 20s linear infinite; /* Adjust the animation duration and timing function as needed */
+            animation: scrollAnimation 20s linear infinite; 
           }
 
           .company {
             flex: 0 0 auto;
-            margin-right: 20px; /* Adjust margin between logos */
           }
 
           .company:last-child {
             margin-right: 0;
           }
-
-          @media (max-width: 768px) {
-            .company-container {
-              overflow-x: auto;
-              scroll-behavior: smooth;
-              width: calc(100% - 40px); /* Adjust width for small screens */
-            }
-
-            .company {
-              margin-right: 10px; /* Adjust margin between logos for small screens */
-            }
-          }
         `}
       </style>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center pb-8 lg:pb-20 ">
+      <div className="w-[400px] md:w-[700px] lg:max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center pb-8 lg:pb-20">
           <p className="mt-2 text-2xl lg:text-3xl leading-8 font-extrabold tracking-tight text-gray-900">
             {subtitle}
           </p>
@@ -72,7 +58,7 @@ const Companies = () => {
             {description}
           </p>
         </div>
-        <div className="company-container mt-16" ref={containerRef}>
+        <div className="company-container w-[400px] md:w-[700px] lg:max-w-7xl mt-16" ref={containerRef}>
           {companies.map((company, index) => (
             <div key={index} className="company flex flex-col items-center mx-8">
               <img
@@ -98,74 +84,3 @@ const Companies = () => {
 };
 
 export default Companies;
-
-
-
-
-
-// import React from 'react';
-// import config from '../config/index.json';
-
-// const Companies = () => {
-//   const { features } = config;
-//   const { subtitle, description, companies } = features;
-
-//   return (
-//     <div className={`py-12 bg-background`} id="features">
-//       <style>
-//         {`
-//           @keyframes marquee {
-//             0% {
-//               transform: translateX(0);
-//             }
-//             100% {
-//               transform: translateX(-100%);
-//             }
-//           }
-
-//           .company-container {
-//             display: flex;
-//             overflow-x: auto;
-//           }
-
-//           .company-container::-webkit-scrollbar {
-//             display: none;
-//           }
-
-//           .company {
-//             flex: 0 0 auto;
-//           }
-
-//           .company:last-child {
-//             margin-right: 0;
-//           }
-//         `}
-//       </style>
-
-//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-//         <div className="text-center pb-8 lg:pb-20 ">
-//           <p className="mt-2 text-2xl lg:text-3xl leading-8 font-extrabold tracking-tight text-gray-900">
-//             {subtitle}
-//           </p>
-//           <p className="mt-4 px-6 lg:px-20 text-sm lg:text-xl text-gray-500">
-//             {description}
-//           </p>
-//         </div>
-//         <div className="company-container mt-16">
-//           {companies.map((company, index) => (
-//             <div key={index} className="company flex flex-col items-center mx-8">
-//               <img
-//                 src={company.logo}
-//                 alt={company.name}
-//                 className="h-36"
-//               />
-//               {/* <h1 className="mt-2 text-xl leading-8 font-extrabold tracking-tight text-gray-900">{company.name}</h1> */}
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Companies;
